e574a1efe2871c2504b7b43de82837b9b6309379
Shwetank <shwetankthakur.risingtechnosoft@gmail.com>
2023-09-26 16:29:27 +0530

    Add project files.

---
+++ Rising.WebLiteProcess/Models/ImportFileInput.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+
+namespace Rising.WebRise.Models
+{  
+    public class ImportFileInput
+    {
+       
+        
+        public string FilePath { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime TradeDate { get; set; }
+
+        [Display(Name = "Import Basis")]
+        public string ImportBasis { get; set; }
+
+        public string FileType { get; set; }
+
+        public string Exchange { get; set; }
+
+        public string FileName { get; set; }
+
+        public string Session { get; set; }
+
+        public string Records { get; set; }
+
+        [Display(Name = "Pro Code")]
+        public string procode { get; set; }
+        [Display(Name = "Broker Code")]
+        public string brokercode { get; set; }
+
+        [Display(Name = "Calculate Brokerage")]
+        public bool calbrok { get; set; }
+
+        [Display(Name = "User Id Wise Transfer")]
+        public string useridwt { get; set; }
+
+        public string Imported { get; set; }
+
+        public string Rejected { get; set; }
+        [Display(Name = "Member ID")]
+        public string memberid { get; set; }
+
+        public string File { get; set; }
+
+        [Display(Name = "Session Id")]
+        public string SessionId { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime Date { get; set; }
+
+        [Display(Name = "Delete Only")]
+        public bool DeleteOnly { get; set; }
+
+
+    }
+}
